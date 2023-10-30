25482ab91617bb24bca57ad616932156a95664b4
Tony Nie <tony.nie@mchrcloud.com>
2023-10-30 12:12:32 +0800

    feat(*): Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # eslint-plugin-layered-import
+
+Enforced layered architecture by checking import statements
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-layered-import`:
+
+```sh
+npm install eslint-plugin-layered-import --save-dev
+```
+
+## Usage
+
+Add `layered-import` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "layered-import"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "layered-import/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+
+| Name                                                   | Description        |
+| :----------------------------------------------------- | :----------------- |
+| [wrong-import-layer](docs/rules/wrong-import-layer.md) | Wrong import layer |
+
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/wrong-import-layer.js
@@ -0,0 +1,105 @@
+/**
+ * @fileoverview Wrong import layer
+ * @author Tony Nie
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/wrong-import-layer"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  }
+});
+ruleTester.run("wrong-import-layer", rule, {
+  valid: [
+    {
+      code: 'import A from "lib/foo"',
+      options: [
+        {
+          layerConfigs: [
+            { layer: 1, path: 'lib/**' },
+            { layer: 2, path: 'components/**' },
+            { layer: 3, path: 'pages/**' },
+          ]
+        },
+        { physicalFilename: '/c/code/src/components/foo.js' }
+      ],
+    },
+    {
+      code: 'import A from "@/lib/foo"',
+      options: [
+        {
+          layerConfigs: [
+            { layer: 1, path: 'lib/**' },
+            { layer: 2, path: 'components/**' },
+            { layer: 3, path: 'pages/**' },
+          ]
+        },
+        { physicalFilename: '/c/code/src/components/foo.js' }
+      ],
+    },
+    {
+      code: 'import A from "#/axios/foo"',
+      options: [
+        {
+          layerConfigs: [
+            { layer: 1, path: 'lib/**' },
+            { layer: 2, path: 'components/**' },
+            { layer: 3, path: 'pages/**' },
+          ]
+        },
+        { physicalFilename: '/c/code/src/components/foo.js' }
+      ],
+    },
+  ],
+  invalid: [
+    {
+      code: 'import A from "pages/foo"',
+      options: [
+        {
+          layerConfigs: [
+            { layer: 1, path: 'lib/**' },
+            { layer: 2, path: 'components/**' },
+            { layer: 3, path: 'pages/**' },
+          ]
+        },
+        {
+          physicalFilename: '/c/code/src/components/foo.js'
+        },
+      ],
+      errors: [{
+        messageId: 'wrongImportLayer',
+      }],
+    },
+    {
+      code: 'import A from "@/pages/foo"',
+      options: [
+        {
+          layerConfigs: [
+            { layer: 1, path: 'lib/**' },
+            { layer: 2, path: 'components/**' },
+            { layer: 3, path: 'pages/**' },
+          ]
+        },
+        {
+          physicalFilename: '/c/code/src/components/foo.js'
+        },
+      ],
+      errors: [{
+        messageId: 'wrongImportLayer',
+      }],
+    },
+  ],
+});
